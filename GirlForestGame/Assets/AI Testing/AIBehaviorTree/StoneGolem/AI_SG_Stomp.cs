b1583d44d4a9b49efa4e6e1ae565224268833e03
leo <leo.buono@ontariotechu.net>
2023-01-15 12:28:38 -0500

    Stone Golem
    Plus cleaning up a bunch of stuff so it will be easier

--- GirlForestGame/Assets/AI Testing/AIBehaviorTree/StoneGolem/AI_SG_Stomp.cs
+++ GirlForestGame/Assets/AI Testing/AIBehaviorTree/StoneGolem/AI_SG_Stomp.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AI_Boar_DashAttack : AI_BaseClass 
+public class AI_SG_Stomp : AI_BaseClass
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //Play the stomp animation and put hitboxes and stuff, probably some particle effects too 
+    }
+
+    ////OnStateUpdate is handled in the AI_BaseClass////
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -17,10 +19,10 @@ public class AI_Boar_DashAttack : AI_BaseClass
     //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
-    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        //Debug.Log("Attack");
-    }
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
 
     // OnStateIK is called right after Animator.OnAnimatorIK()
     //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
