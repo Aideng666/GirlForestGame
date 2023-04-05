d7d2445f21609c1052be42a523825f1fe98a1bf7
Tyler <tyler.wong@ontariotechu.net>
2023-04-05 02:07:36 -0400

    adjusted settings text scale

--- GirlForestGame/Assets/Scripts/UI/MenuFunctionManager.cs
+++ GirlForestGame/Assets/Scripts/UI/MenuFunctionManager.cs
@@ -99,7 +99,7 @@ public class MenuFunctionManager : MonoBehaviour
                 if (i == selectedButtonIndex)
                 {
                     //menuButtons[selectedButtonIndex].transform.localScale = Vector3.one * 1.9f;
-                    menuButtons[i].GetComponent<RectTransform>().localScale = new Vector3(1.5f, 1.5f, 1.5f);
+                    menuButtons[i].GetComponent<RectTransform>().localScale = new Vector3(1.3f, 1.3f, 1.3f);
                 }
                 else
                 {
